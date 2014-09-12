5cd169d742ae306656c3523256287ef8b42db653
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-09-12 14:29:42 -0400

    Updated munged directories to new names.

--- scripts/munge_fah_data.py
+++ scripts/munge_fah_data.py
@@ -7,14 +7,16 @@ import mdtraj as md
 import fahmunge
 import pandas as pd
 
+# Reads in a list of project details from a CSV file with Core17/18 FAH projects and munges them.
+
 projects = pd.read_csv("./projects.csv", index_col=0)
 output_path = "/data/choderalab/fah/munged/"
 
 for iteration in itertools.count():
     for (project, location, pdb) in projects.itertuples():
         print(project, location, pdb)
-        allatom_output_path = os.path.join(output_path, "allatoms/", "%s/" % project)
-        protein_output_path = os.path.join(output_path, "protein/", "%s/" % project)
+        allatom_output_path = os.path.join(output_path, "all-atoms/", "%s/" % project)
+        protein_output_path = os.path.join(output_path, "no-solvent/", "%s/" % project)
         fahmunge.automation.make_path(allatom_output_path)
         fahmunge.automation.make_path(protein_output_path)
         fahmunge.automation.merge_fah_trajectories(location, allatom_output_path, pdb)
